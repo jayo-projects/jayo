3a5bb1a756b52de3546c9048ac99f2a3318efb74
pull-vert <5107035+pull-vert@users.noreply.github.com>
2025-02-02 17:03:02 +0100

    BasicFifoQueue

---
+++ core/src/main/java/jayo/external/BasicFifoQueue.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2025-present, pull-vert and Jayo contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package jayo.external;
+
+import jayo.internal.SinglyLinkedBasicFifoQueue;
+import org.jspecify.annotations.NonNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+/**
+ * A basic FIFO queue that only supports {@link #offer(Object)}, {@link #peek()}, {@link #poll()},
+ * {@link #contains(Object)}, {@link #remove(Object)}, {@link #isEmpty()} and {@link #iterator()}. All other methods
+ * throw {@code UnsupportedOperationException}.
+ * <p>
+ * <b>Be careful</b>, the returned boolean of our {@link #offer(Object)} method does not respect the
+ * {@link Queue#offer(Object)} rationale, it has been adapted to our need. Read its javadoc for details.
+ */
+public sealed interface BasicFifoQueue<T> extends Queue<T> permits SinglyLinkedBasicFifoQueue {
+    static <T> BasicFifoQueue<T> create() {
+        return new SinglyLinkedBasicFifoQueue<>();
+    }
+
+    /**
+     * Inserts the specified element into this queue.
+     *
+     * @return true if element is alone in this queue, meaning this queue was empty before that.
+     * @apiNote This operation's result differs from the {@link Queue#offer(Object)} rationale. Our offer operation
+     * always succeed.
+     */
+    @Override
+    boolean offer(final @NonNull T item);
+
+    @Override
+    T peek();
+
+    @Override
+    T poll();
+
+    @Override
+    boolean isEmpty();
+
+    @Override
+    boolean contains(final @NonNull Object o);
+
+    @Override
+    boolean remove(final @NonNull Object o);
+
+    /**
+     * @return an {@code Iterator} over the elements in this queue in the same order as they were inserted.
+     */
+    @NonNull
+    @Override
+    Iterator<T> iterator();
+
+    @Override
+    default void forEach(final @NonNull Consumer<? super T> action) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default Object @NonNull [] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default <T1> T1 @NonNull [] toArray(final @NonNull T1 @NonNull [] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default <T1> T1 @NonNull [] toArray(final @NonNull IntFunction<T1 @NonNull []> generator) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default boolean containsAll(final @NonNull Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default boolean addAll(final @NonNull Collection<? extends T> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default boolean removeAll(final @NonNull Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default boolean removeIf(final @NonNull Predicate<? super T> filter) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default boolean retainAll(final @NonNull Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default @NonNull Spliterator<T> spliterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default @NonNull Stream<T> stream() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default @NonNull Stream<T> parallelStream() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default boolean add(final @NonNull T t) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default T remove() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default T element() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ core/src/main/java/jayo/internal/SinglyLinkedBasicFifoQueue.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2025-present, pull-vert and Jayo contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package jayo.internal;
+
+import jayo.external.BasicFifoQueue;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public final class SinglyLinkedBasicFifoQueue<T> implements BasicFifoQueue<T> {
+    private static class Node<T> {
+        private final @NonNull T value;
+        private @Nullable Node<T> next = null;
+
+        private Node(final @NonNull T value) {
+            this.value = value;
+        }
+    }
+
+    private @Nullable Node<T> head = null;
+    private @Nullable Node<T> tail = null;
+
+    @Override
+    public boolean offer(final @NonNull T item) {
+        Objects.requireNonNull(item);
+        final var node = new Node<>(item);
+        if (tail != null) {
+            tail.next = node;
+            tail = node;
+            return false;
+        }
+        // queue was empty
+        tail = node;
+        head = node;
+        return true;
+    }
+
+    @Override
+    public T peek() {
+        return head != null ? head.value : null;
+    }
+
+    @Override
+    public T poll() {
+        // queue was empty
+        if (head == null) {
+            return null;
+        }
+        // queue had only one item
+        if (head == tail) {
+            tail = null;
+        }
+        final var value = head.value;
+        final var removed = head;
+        head = head.next;
+        removed.next = null;
+        return value;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public boolean contains(final @NonNull Object o) {
+        Objects.requireNonNull(o);
+        var node = head;
+        while (node != null) {
+            if (node.value.equals(o)) {
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(final @NonNull Object o) {
+        Objects.requireNonNull(o);
+        if (head == null) {
+            return false;
+        }
+        var node = head;
+        if (node.value.equals(o)) {
+            head = node.next;
+            return true;
+        }
+
+        var previous = head;
+        node = head.next;
+        while (node != null) {
+            if (node.value.equals(o)) {
+                if (tail == node) {
+                    tail = previous;
+                }
+                previous.next = node.next;
+                return true;
+            }
+            previous = node;
+            node = node.next;
+        }
+        return false;
+    }
+
+    @Override
+    public @NonNull Iterator<T> iterator() {
+        return head != null ? new SinglyLinkedIterator() : Collections.emptyIterator();
+    }
+
+    private final class SinglyLinkedIterator implements Iterator<T> {
+        private final @NonNull Node<T> virtualOrigin = new Node<>(null); // ok to put null, this first item is virtual
+        private @NonNull Node<T> previous;
+        private @NonNull Node<T> current;
+        private boolean canRemove = false;
+
+        private SinglyLinkedIterator() {
+            previous = virtualOrigin;
+            current = previous;
+            current.next = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            canRemove = false;
+            return current.next != null;
+        }
+
+        @Override
+        public T next() {
+            if (current.next == null) {
+                throw new NoSuchElementException();
+            }
+            previous = current;
+            current = current.next;
+            canRemove = true;
+            return current.value;
+        }
+
+        @Override
+        public void remove() {
+            if (!canRemove) {
+                throw new IllegalStateException();
+            }
+            if (head == current) {
+                head = current.next;
+            }
+            if (tail == current) {
+                tail = previous != virtualOrigin ? previous : null;
+            }
+            previous.next = current.next;
+            current = previous;
+            canRemove = false;
+        }
+    }
+}
