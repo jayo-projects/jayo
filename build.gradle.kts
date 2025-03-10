3f94b45eff5d235510d06f66dfef8a6988a493b4
pull-vert <5107035+pull-vert@users.noreply.github.com>
2025-03-10 12:28:55 +0100

    bump dependencies 10/03/25

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,6 +90,6 @@ configure(listOf(tasks.release, tasks.runBuildTasks)) {
 // when version changes :
 // -> execute ./gradlew wrapper, then remove .gradle directory, then execute ./gradlew wrapper again
 tasks.wrapper {
-    gradleVersion = "8.12.1"
+    gradleVersion = "8.13"
     distributionType = Wrapper.DistributionType.ALL
 }
