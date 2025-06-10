3899da92fdc0716371812389aee19a7bca6055a2
pull-vert <5107035+pull-vert@users.noreply.github.com>
2025-06-10 21:56:02 +0200

    chore: increase koverage

--- build-logic/src/main/kotlin/jayo-commons.gradle.kts
+++ build-logic/src/main/kotlin/jayo-commons.gradle.kts
@@ -20,7 +20,7 @@ fun catalogVersion(lib: String) =
 val isCI = providers.gradleProperty("isCI")
 
 val koverage = mapOf(
-    "jayo" to /*if (isCI.isPresent) 84 else*/ 84,
+    "jayo" to if (isCI.isPresent) 84 else 85,
     "jayo-3p-kotlinx-serialization" to 54,
     "jayo-scheduler" to 84,
 )
