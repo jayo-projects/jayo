1445221f6342861a145f39a9be3de5510f362bb4
pull-vert <5107035+pull-vert@users.noreply.github.com>
2026-01-30 13:49:04 +0100

    bump dependencies 30/01/26

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,6 +90,6 @@ configure(listOf(tasks.release, tasks.runBuildTasks)) {
 // when the Gradle version changes:
 // -> execute ./gradlew wrapper, then remove .gradle directory, then execute ./gradlew wrapper again
 tasks.wrapper {
-    gradleVersion = "9.3.0"
+    gradleVersion = "9.3.1"
     distributionType = Wrapper.DistributionType.ALL
 }
